a405f66e8642e3fb9becc3dcaf0a1dfab1cc3afb
fran-valls <fjvalls@gmail.com>
2023-05-29 11:33:25 +0200

    Modificación Base de datos

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -3,33 +3,33 @@
 
  use favorSwapp;
 
+-- Tabla provincias
  CREATE TABLE provincias (
 	id     VARCHAR(2)  NOT NULL PRIMARY KEY,
 	nombre VARCHAR(50) NOT NULL /*Usaremos 00 para indicar nombre = '' */
 ) ENGINE=INNODB;
 
+-- Tabla direcciones
+create table direcciones (
+    id INT (4) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    direccion VARCHAR(200) NOT NULL,
+    id_provincia VARCHAR (2),
+    -- Este campo id_provincia hace referencia a la id de provincias
+    FOREIGN KEY (id_provincia) REFERENCES provincias (id)
+    ) ENGINE = InnoDB;
 
- 
+-- Tabla usuarios
  create table usuarios (
     id INT (4) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     nombre VARCHAR(25) NOT NULL,
     apellido1 VARCHAR(25) NOT NULL,
     apellido2 VARCHAR(25),
-    edad INT(2),
-    id_provincia VARCHAR (2),
+    fecha_nacimiento DATETIME,
+    id_direccion INT (4),
+    is_Admin BOOLEAN DEFAULT 0,
     -- Este campo id_provincias hace referencia a la id de provincias
-    FOREIGN KEY (id_provincia) REFERENCES provincias (id)
+    FOREIGN KEY (id_direccion) REFERENCES direcciones (id)
     ) ENGINE = InnoDB;
 
 
-create table direcciones (
-    id INT (4) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    direccion VARCHAR(50) NOT NULL,
-    id_usuario INT (4),
-    id_provincia VARCHAR (2),
-    -- Este campo id_provincia hace referencia a la id de provincias
-    FOREIGN KEY (id_provincia) REFERENCES provincias (id),
-    -- Este campo id_usuario hace referencia a la id de usuario
-    FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
-    ) ENGINE = InnoDB;
-    
+
