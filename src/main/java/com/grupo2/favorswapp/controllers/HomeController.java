83810e1371c653d3449a9e21f43380241623d643
Java <fjvalls@gmail.com>
2023-07-13 18:41:53 +0200

    Añadido Javadoc a HomeController

--- src/main/java/com/grupo2/favorswapp/controllers/HomeController.java
+++ src/main/java/com/grupo2/favorswapp/controllers/HomeController.java
@@ -1,16 +1,15 @@
 package com.grupo2.favorswapp.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controlador HomeController. Al iniciar el programa lo redirige a "login.html"
+ */
 @Controller
 public class HomeController {
-
     @RequestMapping("/")
     public String home(){
         return "login";
     }
-
-
 }
